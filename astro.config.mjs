117c229484b7327d76b624bcbf84b778ae3bd3b1
namish <namish@tuta.io>
2025-06-05 18:42:42 +0530

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  })],
+  vite: { plugins: [tailwindcss()], },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        dark: 'gruvbox-dark-medium',
+        light: 'gruvbox-light-medium'
+      },
+      wrap: true
+    }
+  }
+});
